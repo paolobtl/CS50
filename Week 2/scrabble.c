bf34228a6403e1dc029253e8fac8fee5f97df752
Paolo <68049947+paolobtl@users.noreply.github.com>
2022-06-03 13:44:27 +0200

    Create scrabble.c

---
+++ Week 2/scrabble.c
@@ -0,0 +1,67 @@
+/*********************************************************************
+  * Assignment: https://cs50.harvard.edu/x/2022/labs/2/              *
+  * Goal: Each word has an assigned value,                           *  
+  *       return the player who has more points                      *
+  *                                                                  *
+  * @ Paolo Bietolini                                                *
+  * 2022                                                             *
+  ********************************************************************/
+
+
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    if (score1 > score2)
+    {
+        printf("Player 1 Wins\n");
+    } 
+    else if (score1 < score2) 
+    {
+        printf("Player 2 Wins\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    int len = strlen(word);
+    int score = 0;
+
+    //Iterates over the word and subtracts the ASCII value of each letter to 97 or 65
+    //this will return the index of the array POINTS and can be used to calculate the score
+    for (int i = 0; i < len; i++)
+    { 
+        if (isalpha(word[i]))
+        {
+            int x = (isupper(word[i])) ? 65 : 97;
+            int index = (int)word[i] - x;
+            score += POINTS[index];
+        }
+        //If the charachter is not a letter then no points are assigned
+        else
+        {
+            score += 0;
+        }
+    }
+    return score;
+}
